d3db84d4ffb5b2de3a74c792f8696bda7f008fc0
Patrick Horne <35867466+Paddylabs@users.noreply.github.com>
2018-07-27 10:18:54 +0100

    Commit

---
+++ PKILab.ps1
@@ -0,0 +1,342 @@
+﻿# Stop the script if a fatal error occurs
+$ErrorActionPreference = 'Stop'
+
+Configuration BasicServerClient {
+<#
+    Requires the following custom DSC resources:
+        xComputerManagement: https://github.com/PowerShell/xComputerManagement
+        xNetworking:         https://github.com/PowerShell/xNetworking
+        xActiveDirectory:    https://github.com/PowerShell/xActiveDirectory
+        xSmbShare:           https://github.com/PowerShell/xSmbShare
+        xDhcpServer:         https://github.com/PowerShell/xDhcpServer
+        xDnsServer:          https://github.com/PowerShell/xDnsServer
+#>
+    param (
+        [Parameter()] [ValidateNotNull()] [PSCredential] $Credential = (Get-Credential -Credential 'Administrator')
+    )
+    Import-DscResource -Module xComputerManagement, xNetworking, xActiveDirectory;
+    Import-DscResource -Module xSmbShare, PSDesiredStateConfiguration;
+    Import-DscResource -Module xDHCPServer, xDnsServer;
+    Import-DscResource -Module cNTFSAccessControl;
+
+    node $AllNodes.Where({$true}).NodeName {
+
+        LocalConfigurationManager {
+
+            RebootNodeIfNeeded   = $true;
+            AllowModuleOverwrite = $true;
+            ConfigurationMode    = 'ApplyOnly';
+            # CertificateID        = $node.Thumbprint;
+        }
+
+        if (-not [System.String]::IsNullOrEmpty($node.IPAddress)) {
+
+            xIPAddress 'PrimaryIPAddress' {
+
+                IPAddress      = $node.IPAddress;
+                InterfaceAlias = $node.InterfaceAlias;
+                AddressFamily  = $node.AddressFamily;
+            }
+
+            if (-not [System.String]::IsNullOrEmpty($node.DefaultGateway)) {
+
+                xDefaultGatewayAddress 'PrimaryDefaultGateway' {
+
+                    InterfaceAlias = $node.InterfaceAlias;
+                    Address        = $node.DefaultGateway;
+                    AddressFamily  = $node.AddressFamily;
+                }
+            }
+
+            if (-not [System.String]::IsNullOrEmpty($node.DnsServerAddress)) {
+
+                xDnsServerAddress 'PrimaryDNSClient' {
+
+                    Address        = $node.DnsServerAddress;
+                    InterfaceAlias = $node.InterfaceAlias;
+                    AddressFamily  = $node.AddressFamily;
+                }
+            }
+
+            if (-not [System.String]::IsNullOrEmpty($node.DnsConnectionSuffix)) {
+
+                xDnsConnectionSuffix 'PrimaryConnectionSuffix' {
+
+                    InterfaceAlias           = $node.InterfaceAlias;
+                    ConnectionSpecificSuffix = $node.DnsConnectionSuffix;
+                }
+            }
+
+        } #end if IPAddress
+
+        
+    } #end nodes ALL
+
+    node $AllNodes.Where({$_.Role -in 'DC'}).NodeName {
+
+        ## Flip credential into username@domain.com
+        $domainCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ("$($Credential.UserName)@$($node.DomainName)", $Credential.Password);
+
+        xComputer 'Hostname' {
+
+            Name = $node.NodeName;
+        }
+
+        ## Hack to fix DependsOn with hypens "bug" :(
+        foreach ($feature in @(
+                'AD-Domain-Services',
+                'GPMC',
+                'RSAT-AD-Tools',
+                'DHCP',
+                'RSAT-DHCP'
+            )) {
+            WindowsFeature $feature.Replace('-','') {
+
+                Ensure               = 'Present';
+                Name                 = $feature;
+                IncludeAllSubFeature = $true;
+            }
+        }
+
+        xADDomain 'ADDomain' {
+
+            DomainName                    = $node.DomainName;
+            SafemodeAdministratorPassword = $Credential;
+            DomainAdministratorCredential = $Credential;
+            DependsOn                     = '[WindowsFeature]ADDomainServices';
+        }
+
+        # Convert the domain name to the distinguished name
+        $DomainDN = ('DC=' + $($node.DomainName).Replace('.',',DC='))
+        $BaseOU   = "OU=$($ConfigurationData.NonNodeData.OrganisationName),$($DomainDN)"
+
+        # Create an OU called 'PKILAB'
+        xADOrganizationalUnit 'OU_BaseOU' {
+            Name = $ConfigurationData.NonNodeData.OrganisationName
+            Path = $DomainDN
+
+            }
+
+        # Create a 'Lab Users' OU under the base OU
+        xADOrganizationalUnit 'OU_LabUsers' {
+            Name = 'Lab Users'
+            Path = $BaseOU
+        }
+
+        # Create OUs for each Role under the base OU
+        xADOrganizationalUnit 'OU_LabComputers' {
+            Name = 'Lab Computers'
+            Path = $BaseOU
+        }
+
+        xADOrganizationalUnit 'OU_PKI' {
+            Name = 'PKI Servers'
+            Path = $BaseOU
+        }
+
+        xDhcpServerAuthorization 'DhcpServerAuthorization' {
+
+            Ensure    = 'Present';
+            DependsOn = '[WindowsFeature]DHCP','[xADDomain]ADDomain';
+        }
+
+        xDhcpServerScope 'DhcpScope10_0_0_0' {
+
+            Name          = 'Corpnet';
+            IPStartRange  = '10.0.0.100';
+            IPEndRange    = '10.0.0.200';
+            SubnetMask    = '255.255.255.0';
+            LeaseDuration = '00:08:00';
+            State         = 'Active';
+            AddressFamily = 'IPv4';
+            DependsOn     = '[WindowsFeature]DHCP';
+        }
+
+        xDnsServerADZone 'PaddylabZone' {
+
+            Name             = 'paddylab.net';
+            ReplicationScope = 'Forest';
+            Ensure           = 'Present';
+            DependsOn        = '[xADDomain]ADDomain';
+                     
+        
+        }
+
+        xDnsRecord 'PKI' {
+        
+            Name = 'PKI'
+            Zone = 'paddylab.net'
+            Type = 'CName'
+            Target = 'web01.corp.paddylab.net'
+            Ensure = 'present'
+            DependsOn = '[xDnsServerADZone]PaddylabZone'
+        
+        }
+
+        xDhcpServerOption 'DhcpScope10_0_0_0_Option' {
+
+            ScopeID            = '10.0.0.0';
+            DnsDomain          = 'corp.contoso.com';
+            DnsServerIPAddress = '10.0.0.1';
+            Router             = '10.0.0.2';
+            AddressFamily      = 'IPv4';
+            DependsOn          = '[xDhcpServerScope]DhcpScope10_0_0_0';
+        }
+
+        xADUser User1 {
+
+            DomainName  = $node.DomainName;
+            UserName    = 'User1';
+            Description = 'PKI Test Lab user';
+            Path        = "OU=Lab Users,$BaseOU"
+            Password    = $Credential;
+            Ensure      = 'Present';
+            DependsOn   = '[xADDomain]ADDomain';
+        }
+
+        xADGroup DomainAdmins {
+
+            GroupName        = 'Domain Admins';
+            MembersToInclude = 'User1';
+            DependsOn        = '[xADUser]User1';
+        }
+
+        xADGroup EnterpriseAdmins {
+
+            GroupName        = 'Enterprise Admins';
+            GroupScope       = 'Universal';
+            MembersToInclude = 'User1';
+            DependsOn        = '[xADUser]User1';
+        }
+
+    } #end nodes DC
+
+    node $AllNodes.Where({$_.Role -in 'CLIENT','WEB','SubCA'}).NodeName {
+
+        ## Flip credential into username@domain.com
+        $upn = '{0}@{1}' -f $Credential.UserName, $node.DomainName;
+        $domainCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($upn, $Credential.Password);
+
+        xComputer 'DomainMembership' {
+
+            Name       = $node.NodeName;
+            DomainName = $node.DomainName;
+            Credential = $domainCredential;
+        }
+    } #end nodes DomainJoined
+
+
+    node $AllNodes.Where({$_.Role -in 'ROOTCA','SubCA'}).NodeName {
+
+        foreach ($feature in @(
+                'Adcs-cert-Authority',
+                'RSAT-ADCS-Mgmt'
+                )) {
+            WindowsFeature $feature.Replace('-','') {
+
+                Ensure               = 'Present';
+                Name                 = $feature;
+                IncludeAllSubFeature = $true;
+            }
+
+        
+    } #end nodes ROOTCA
+
+    
+
+}   
+
+   node $AllNodes.Where({$_.Role -eq 'WEB'}).NodeName {
+
+        foreach ($feature in @(
+                'WEB-SERVER'
+                )) {
+            WindowsFeature $feature.Replace('-','') {
+
+                Ensure               = 'Present';
+                Name                 = $feature;
+                IncludeAllSubFeature = $true;
+            }
+            
+            File 'FilesFolder' {
+
+            DestinationPath = 'C:\PKI';
+            Type            = 'Directory';
+        }
+
+            xSmbShare 'FilesShare' {
+
+            Name         = 'PKI';
+            Path         = 'C:\PKI';
+            ChangeAccess = 'CORP\User1',"corp\cert publishers";
+            DependsOn    = '[File]FilesFolder';
+            Ensure       = 'Present'
+        }
+
+        cNtfsPermissionEntry PermissionSet1 {
+            
+            Ensure = 'Present'
+            Path = 'C:\PKI'
+            Principal = "corp\cert publishers"
+            AccessControlInformation = @(
+                cNtfsAccessControlInformation
+                {
+                    AccessControlType = 'Allow'
+                    FileSystemRights = 'Modify'
+                    Inheritance = 'ThisFolderSubfoldersAndFiles'
+                    NoPropagateInherit = $false
+                }
+            )
+            DependsOn = '[File]FilesFolder'
+
+            }
+
+#            cNtfsPermissionEntry PermissionSet2 {
+            
+#            Ensure = 'Present'
+#            Path = 'C:\PKI'
+#            Principal = "corp\cert publishers"
+#            AccessControlInformation = @(
+#                cNtfsAccessControlInformation
+#                {
+#                    AccessControlType = 'Allow'
+#                    FileSystemRights = 'Modify'
+#                    Inheritance = 'ThisFolderSubfoldersAndFiles'
+#                    NoPropagateInherit = $false
+#                }
+#            )
+#            DependsOn = '[File]FilesFolder'
+#
+#            }
+
+        
+    } #end nodes WEB
+
+    
+
+}   
+ 
+
+} #end Configuration Example
+
+# Use the psd1 in the same folder as this script
+$ConfigData = "$(Split-Path $MyInvocation.MyCommand.Path)\PKILab.psd1"
+
+# Create a new credential that we'll pass to BasicServerClient (Required when creating the domain & joining the domain) and to Start-LabConfiguration
+$AdministratorCredential = [pscredential]::new('Administrator', ('Password1' | ConvertTo-SecureString -AsPlainText -Force))
+
+# Generate the .MOF files that will be injected into our VMs and used to set them up
+Write-Host 'Generating MOFs' -ForegroundColor Green
+BasicServerClient -ConfigurationData $ConfigData -OutputPath 'C:\Lability\Configurations' -Credential $AdministratorCredential -Verbose
+
+# Verify lab configuration & see what parts of it already exist (if any)
+Write-Host 'Verifying lab configuration' -ForegroundColor Green
+Test-LabConfiguration -ConfigurationData $ConfigData  -Verbose
+
+# Create the lab from our config
+Write-Host 'Creating lab' -ForegroundColor Green
+Start-LabConfiguration -ConfigurationData $ConfigData -Verbose -IgnorePendingReboot -Credential $AdministratorCredential
+
+# And once it's created, start the lab environment
+Write-Host 'Starting PKI Test Lab' -ForegroundColor Green
+Start-Lab -ConfigurationData $ConfigData -Verbose
---
+++ PKILab.psd1
@@ -0,0 +1,186 @@
+﻿@{
+    AllNodes = @(
+        @{
+            NodeName                    = '*';
+            InterfaceAlias              = 'Ethernet';
+            DefaultGateway              = '10.0.0.254';
+            PrefixLength                = 24;
+            AddressFamily               = 'IPv4';
+            DnsServerAddress            = '10.0.0.1';
+            DomainName                  = 'corp.paddylab.net';
+            PSDscAllowPlainTextPassword = $true;
+            #CertificateFile            = "$env:AllUsersProfile\Lability\Certificates\LabClient.cer";
+            #Thumbprint                 = 'AAC41ECDDB3B582B133527E4DE0D2F8FEB17AAB2';
+            PSDscAllowDomainUser        = $true; # Removes 'It is not recommended to use domain credential for node X' messages
+            Lability_SwitchName         = 'Corpnet';
+            Lability_ProcessorCount     = 1;
+            Lability_StartupMemory      = 2GB;
+            Lability_Media              = '2016_x64_Standard_EN_Eval';
+        }
+        @{
+            NodeName                = 'DC1';
+            IPAddress               = '10.0.0.1';
+            DnsServerAddress        = '127.0.0.1';
+            Role                    = 'DC';
+            Lability_ProcessorCount = 2;
+        }
+        
+        @{
+            NodeName  = 'ROOTCA01';
+            IPAddress = '10.0.0.10';
+            Role      = 'ROOTCA';
+            Lability_Resource = 'Root_CAPolicy','Root_CASetup','Root_CAConfig'
+            
+        }
+
+        @{
+            NodeName  = 'SubCA01';
+            IPAddress = '10.0.0.11';
+            Role      = 'SubCA';
+            Lability_Resource = 'Sub_CAPolicy','Sub_CASetup','Sub_CAConfig'
+
+        }
+
+#        @{
+#            NodeName  = 'SubCA02';
+#            IPAddress = '10.0.0.12';
+#            Role      = 'SubCA';
+#            Lability_Resource = 'Sub_CAPolicy','Sub_CASetup','Sub_CAConfig'
+#
+#        }
+
+
+       @{
+            NodeName  = 'WEB01';
+            IPAddress = '10.0.0.13';
+            Role      = 'WEB';
+            Lability_Resource = 'PKI_IIS_Config'
+        }
+        
+#        @{
+#            NodeName       = 'CLIENT1';
+#            Role           = 'CLIENT';
+#            Lability_Media = 'WIN10_x64_Enterprise_EN_Eval';
+#            Lability_Resource = 'RSAT_2016';
+#            <# Lability_CustomBootStrap = 'Now implemented in the Media's CustomData.CustomBootstrap property #>
+#        }
+        
+    );
+    NonNodeData = @{
+        OrganisationName = 'PKILab'
+        Lability = @{
+            EnvironmentPrefix = 'PKILab-';
+            Media = @();
+            Network = @(
+                @{ Name = 'Corpnet'; Type = 'Internal'; }
+                # @{ Name = 'Corpnet'; Type = 'External'; NetAdapterName = 'Ethernet'; AllowManagementOS = $true; }
+                <#
+                    IPAddress: The desired IP address.
+                    InterfaceAlias: Alias of the network interface for which the IP address should be set. <- Use NetAdapterName
+                    DefaultGateway: Specifies the IP address of the default gateway for the host. <- Not needed for internal switch
+                    Subnet: Local subnet CIDR (used for cloud routing).
+                    AddressFamily: IP address family: { IPv4 | IPv6 }
+                #>
+            );
+
+           Resource = @(
+                @{  
+                    # Resource Identifier. If the resource is to be expanded (ISO or ZIP), it will be expanded into
+                    # the \Resources\<Resource Id> folder on the target node.
+                    Id = 'RSAT_2016';
+
+                    # When the file is downloaded it will be placed into the Host resources folder with this filename
+                    Filename = 'WindowsTH-RSAT_WS_1709-x64.msu';
+
+                    # The source URI. Must be http, https or file URI.  If the path includes spaces they must be URL encoded.
+                    # Uri = 'file://C:\Lability\Resources\WindowsTH-RSAT_WS_1709-x64.msu'
+
+                    # If you want the module to check the downloaded file you can specify an MD5 Checksum.  Make sure this is
+                    # correct or the module will continously try to download the file.
+                    # Checksum = '';
+
+                    # If the resource is a .zip or .iso file it can be expanded / decompressed when copied into the node's 
+                    # \Resources\<Resource Id> folder. If no specified this value defaults to false.
+                    # Expand = $True;
+
+                    # The default Target path can be overridden by specifiying a destination path. This path is relative to
+                    # the OS System drive.
+                    # DestinationPath = '\Source'
+
+                    }
+
+                @{
+                    Id = 'Root_CAPolicy'
+                    Filename = 'CAPolicy.inf'
+                    DestinationPath = '\Windows'
+                    Uri = 'file://C:\Lability\Resources\PKI\RootCA\Root_CAPolicy.inf'
+
+                    }
+
+                @{
+                    Id = 'Root_CASetup'
+                    Filename = 'Root_CA_Setup.ps1'
+                    Uri = 'file://C:\Lability\Resources\PKI\RootCA\Root_CA_Setup.ps1'
+
+                    }
+
+                 @{
+                    Id = 'Root_CAConfig'
+                    Filename = 'Root_CA_Config.ps1'
+                    Uri = 'file://C:\Lability\Resources\PKI\RootCA\Root_CA_Config.ps1'
+
+                    }
+
+                @{
+                    Id = 'Sub_CAPolicy'
+                    Filename = 'Sub_CAPolicy.inf'
+                    DestinationPath = '\Windows'
+                    Uri = 'file://C:\Lability\Resources\PKI\SubCA\SubCA_CAPolicy.inf'
+
+                    }
+
+                @{
+                    Id = 'Sub_CASetup'
+                    Filename = 'Sub_CA_Setup.ps1'
+                    Uri = 'file://C:\Lability\Resources\PKI\SubCA\Sub_CA_Setup.ps1'
+
+                    }
+
+                 @{
+                    Id = 'Sub_CAConfig'
+                    Filename = 'Sub_CA_Config.ps1'
+                    Uri = 'file://C:\Lability\Resources\PKI\SubCA\Sub_CA_Config.ps1'
+
+                    }
+
+                
+                @{
+                    Id = 'PKI_IIS_Config'
+                    Filename = 'PKI_IIS_Config.ps1'
+                    Uri = 'file://C:\Lability\Resources\PKI\Web\PKI_IIS_Config.ps1'
+
+                    }
+                
+            )
+
+            <#
+                If you are generating the .mof files on the host and/or you want Labilty to use the DSC
+                resource versions/modules installed on the physical host, you should remove the 'DSCResource' key.
+                Lability will then "just" copy all local DSC resources.
+            #> 
+            DSCResource = @(
+                ## Download published version from the PowerShell Gallery
+                @{ Name = 'xComputerManagement'; MinimumVersion = '1.9.0.0'; Provider = 'PSGallery'; }
+                ## If not specified, the provider defaults to the PSGallery.
+                @{ Name = 'xSmbShare'; MinimumVersion = '2.1.0.0'; }
+                @{ Name = 'xNetworking'; MinimumVersion = '3.2.0.0'; }
+                @{ Name = 'xActiveDirectory'; MinimumVersion = '2.16.0.0'; }
+                @{ Name = 'xDnsServer'; MinimumVersion = '1.11.0.0'; }
+                @{ Name = 'xDhcpServer'; MinimumVersion = '1.5.0.0'; }
+                @{ Name = 'cNtfsAccessControl'; MinimumVersion = '1.3.1'; }
+                ## The 'GitHub# provider can download modules directly from a GitHub repository, for example:
+                ## @{ Name = 'Lability'; Provider = 'GitHub'; Owner = 'VirtualEngine'; Repository = 'Lability'; Branch = 'dev'; }
+            );
+        };
+    };
+};
